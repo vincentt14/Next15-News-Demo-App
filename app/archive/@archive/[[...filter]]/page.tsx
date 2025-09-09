bd6cf0cbfb1948b1e069a905f0ed9e89c3c8cbac
vincentt14 <stefanuslim844@gmail.com>
2025-09-09 23:46:21 +0700

    +. understanding and fixing multiple path segments in a catch all fallback routes

--- app/archive/@archive/[[...filter]]/page.tsx
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -1,9 +1,9 @@
 import { NewsList } from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import Link from "next/link";
 
 type FilteredNewsPageProps = {
-  params: { filter: number };
+  params: { filter?: string[] };
 };
 
 // kegunaan [[...filter]] -> file page.tsx yg ada di dalamnya akan aktif utk semua path segment setelah archive
@@ -13,25 +13,49 @@ type FilteredNewsPageProps = {
 
 // maka filter akan berisi array atas semua segment yang sesuai
 
-export default function FilteredNewsPage({ params }: FilteredNewsPageProps) {
-  const { filter } = params;
-  console.log(filter);
+export default async function FilteredNewsPage({ params }: FilteredNewsPageProps) {
+  const { filter } = await params;
 
-  const links = getAvailableNewsYears();
-  // const news = getNewsForYear(filter);
+  const selectedYear = filter?.[0] ? Number(filter[0]) : undefined;
+  const selectedMonth = filter?.[1] ? Number(filter[1]) : undefined;
 
-  // return <NewsList news={news} />;
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 }
